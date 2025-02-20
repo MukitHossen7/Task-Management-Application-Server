d048f7f85aa8df28e334ce93a2a833173d53a398
MukitHossen7 <hossenmukit7@gmail.com>
2025-02-20 23:39:08 +0600

    Add production URL to CORS configuration

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ connection();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://task-management-applicat-d6752.web.app",
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
   })
